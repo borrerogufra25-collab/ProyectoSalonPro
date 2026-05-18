400a13e9285495fe59b8c8c21caacb8626a11c7d
Francisco Borrero Guerrero <borrero.gufra25@triana.salesianos.edu>
2026-05-18 17:40:23 +0200

    cambio menor

--- SalonPro/src/main/java/com/salesianostriana/dam/salonpro/DataSeed.java
+++ SalonPro/src/main/java/com/salesianostriana/dam/salonpro/DataSeed.java
@@ -1,6 +1,7 @@
 package com.salesianostriana.dam.salonpro;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.stereotype.Component;
 
@@ -18,23 +19,59 @@ public class DataSeed {
 
 	@PostConstruct
 	public void run() {
-		Cliente c = Cliente.builder()
-				.nombre("Pepito")
-				.telefono("696322304")
-				.email("futa@pera.com")
-				.cumple(LocalDate.of(1998, 05, 11))
-				.build();
 
-		clienteRepositorio.save(c);
+		List<Cliente> listaClientes = List.of(
 
-		Cliente c2 = Cliente.builder()
-				.nombre("María")
-				.telefono("600111222")
-				.email("maria@ejemplo.com")
-				.cumple(LocalDate.of(1995, 8, 25))
-				.build();
+				Cliente.builder()
+						.nombre("Pepito")
+						.telefono("696322304")
+						.email("futa@pera.com")
+						.cumple(LocalDate.of(1998, 05, 11))
+						.build(),
 
-		clienteRepositorio.save(c2);
+				Cliente.builder()
+						.nombre("Michael Scott")
+						.telefono("600998877")
+						.email("michael.scott@dundermifflin.com")
+						.cumple(LocalDate.of(1964, 3, 15))
+						.build(),
+
+				Cliente.builder()
+						.nombre("Dwight Schrute")
+						.telefono("611223344")
+						.email("dwight.schrute@dundermifflin.com")
+						.cumple(LocalDate.of(1970, 1, 20))
+						.build(),
+
+				Cliente.builder()
+						.nombre("Jim Halpert")
+						.telefono("622334455")
+						.email("jim.halpert@dundermifflin.com")
+						.cumple(LocalDate.of(1978, 10, 1))
+						.build(),
+
+				Cliente.builder()
+						.nombre("Walter White")
+						.telefono("600112233")
+						.email("heisenberg@abq.com")
+						.cumple(LocalDate.of(1958, 9, 7))
+						.build(),
+
+				Cliente.builder()
+						.nombre("Jesse Pinkman")
+						.telefono("622334455")
+						.email("capncook@abq.com")
+						.cumple(LocalDate.of(1984, 9, 24))
+						.build(),
+
+				Cliente.builder()
+						.nombre("Saul Goodman")
+						.telefono("699887766")
+						.email("bettercallsaul@law.com")
+						.cumple(LocalDate.of(1960, 11, 12))
+						.build());
+		clienteRepositorio.saveAll(listaClientes);
+
 	}
 
 }
